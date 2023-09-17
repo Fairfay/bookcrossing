e8268dbf71645e3ae5becb06beaf196d5619bf09
Bazulenkov <anton@bazul.ru>
2023-09-17 21:02:49 +0400

    feat: add namespaces and names to urls.

--- bookcrossing_project/catalog/urls.py
+++ bookcrossing_project/catalog/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'catalog'
+
 urlpatterns = [
-    path('categories/', views.categories_view),
-    path('categories/<int:id>/', views.categories_detail_view),
-    path('books/<int:id>/', views.book_detail_view),
-    path('', views.index_view)
+    path('categories/', views.categories_view, name='categories'),
+    path('categories/<int:id>/', views.categories_detail_view, name='category_detail'),
+    path('books/<int:id>/', views.book_detail_view, name='book_detail'),
+    path('', views.index_view, name='index')
 ]
--- bookcrossing_project/pages/urls.py
+++ bookcrossing_project/pages/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pages'
+
 urlpatterns = [
-    path('about/', views.about_view),
+    path('about/', views.about_view, name='about'),
 ]
