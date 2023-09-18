3308dc7427b8f8150fcf22c148f334e832fd4a60
Bazulenkov <anton@bazul.ru>
2023-09-18 21:05:34 +0400

    feat: add data from database to index.html.

--- bookcrossing_project/catalog/views.py
+++ bookcrossing_project/catalog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from data import BOOKS
+
 
 def index_view(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'books': BOOKS
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def categories_view(request):
